4a9a9b0d821b7e409411c5c011471605d70c78bc
thijsgraat <thijs1.graat@gmail.com>
2026-09-04 16:39:17 +0200

    added arguments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMaker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMaker.java
@@ -1,4 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 public class AutonomousMaker {
+
+    public void AutonomousPoint(double x, double y, double phi, double startX, double startY, double startPhi) {
+
+    }
 }
